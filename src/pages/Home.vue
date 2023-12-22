8570d2edffd41577a29994f590c23e96c23ad26f
Jomm95 <venturini.thomas95@gmail.com>
2023-12-22 18:28:08 +0100

    renaming pages

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,20 @@
 <template>
+    <!-- Main container for the component -->
     <div class="select-pokemon-page">
-      <img
-        src="./../assets/logo-pixel.gif"
-        alt="pokémon"
-      >
+      <!-- Pokémon logo image -->
+      <img src="./../assets/logo-pixel.gif" alt="pokémon" />
+      
+      <!-- Container for Pokémon List and Summary Favorites components -->
       <div class="select-pokemon-content">
+        <!-- Pokémon List component -->
         <PokemonList
           :pokemon-list="statePokemonDataList"
           :favorites="stateFavoritePokemonList"
           @deleteFavorite="deleteFavorite"
           @addFavorite="addFavorite"
         />
+  
+        <!-- Summary Favorites component -->
         <SummaryFavorites
           :pokemon-list="statePokemonDataList"
           :favorites="stateFavoritePokemonList"
@@ -22,36 +26,47 @@
   </template>
   
   <script>
+  // Import necessary components and functions from Vue and Vuex
   import PokemonList from '@/components/PokemonList.vue';
   import SummaryFavorites from '@/components/SummaryFavorites.vue';
   import { computed, ref, onMounted } from 'vue';
   import { useStore } from 'vuex';
   
   export default {
+    // Declare components used in this component
     components: {
       PokemonList,
       SummaryFavorites,
     },
+    // Define the component logic using the setup function of Vue 3
     setup() {
+      // Access the Vuex store
       const store = useStore();
+  
+      // Reactive variables for state management
       const statePokemonDataList = computed(() => store.state.statePokemonDataList);
       const stateFavoritePokemonList = computed(() => store.state.stateFavoritePokemonList);
   
+      // Lifecycle hook: runs when the component is mounted
       onMounted(async () => {
+        // Fetch Pokémon data and set it in the Vuex store
         const pokemonData = await getPokemonData();
         setPokemonData(pokemonData);
       });
   
+      // Asynchronous function to fetch Pokémon data
       const getPokemonData = async () => {
         const data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
         const json = await data.json();
         return json.results;
       };
   
+      // Function to set Pokémon data in the Vuex store
       const setPokemonData = (list) => {
         store.dispatch('setPokemonData', list);
       };
   
+      // Functions to perform actions related to favorite Pokémon
       const addFavorite = (payload) => {
         store.dispatch('addFavorite', payload);
       };
@@ -64,6 +79,7 @@
         store.dispatch('eraseFavoritePokemonList');
       };
   
+      // Return variables and functions for use in the template
       return {
         statePokemonDataList,
         stateFavoritePokemonList,
@@ -73,19 +89,20 @@
         deleteFavorite,
         eraseFavoritePokemonList,
       };
-    },
-  };
+    }
+  }
   </script>
   
   <style scoped>
-  .select-pokemon-page {
-    text-align: center;
-  }
+    /* Styling specific to this component */
+    .select-pokemon-page {
+      text-align: center;
+    }
   
-  .select-pokemon-content {
-    text-align: left;
-    display: flex;
-    justify-content: space-evenly;
-  }
+    .select-pokemon-content {
+      text-align: left;
+      display: flex;
+      justify-content: space-evenly;
+    }
   </style>
   
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './pages/myHome.vue';
-import Favorites from './pages/favoritesList.vue';
+import Home from './pages/Home.vue';
+import Favorites from './pages/Favorites.vue';
 
 const routes = [
   {
@@ -10,7 +10,7 @@ const routes = [
   },
   {
     path: '/favorites',
-    name: 'favoritesList',
+    name: 'favorites',
     component: Favorites,
   },
 ];
